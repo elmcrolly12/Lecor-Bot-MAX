0d6481b0634fa549ef415798d193d33c82d72a81
Santanamontaho <138260914+Santanamontaho@users.noreply.github.com>
2023-07-19 19:33:28 -0500

    de 2.68 kb
    Propietario

---
+++ plugins/herramientas-chatGPT.js
@@ -0,0 +1,31 @@
+import fetch from 'node-fetch'
+import cheerio from 'cheerio'
+import gpt from 'api-dylux'
+let handler = async (m, { text, usedPrefix, command }) => {
+if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘾𝙃𝘼𝙏𝙂𝙋𝙏\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`    
+try {
+await conn.sendPresenceUpdate('composing', m.chat)
+//let jailbreak = await fetch('https://raw.githubusercontent.com/Skidy89/chat-gpt-jailbreak/main/Text.txt').then(v => v.text());
+//var syms = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD.\n${jailbreak}`
+let syms = `Actuaras como un Bot de WhatsApp el cual fue creado por GataNina-Li, tu seras GataBot-MD.`
+let res = await gpt.ChatGpt(text, syms)
+await m.reply(res.text)
+} catch {
+try {   
+let IA2 = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)  
+let IAR2 = await IA2.json()
+m.reply(`${IAR2.response}`.trim())    
+} catch {
+try {   
+let rrEes = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
+let jjJson = await rrEes.json()
+m.reply(jjJson.data.data.trim())    
+} catch {      
+try {    
+let tioress = await fetch(`https://api.lolhuman.xyz/api/openai-turbo?apikey=${lolkeysapi}&text=${text}`)
+let hasill = await tioress.json()
+m.reply(`${hasill.result}`.trim())   
+} catch {        
+}}}}} 
+handler.command = ['openai', 'chatgpt', 'ia', 'robot']
+export default handler
